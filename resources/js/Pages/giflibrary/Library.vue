b6e6517a387c71df86496b115bc8e54024b093fa
cloe69 <skeatescole@gmail.com>
2024-08-09 11:35:23 +0100

    added images

--- resources/js/Pages/giflibrary/Library.vue
+++ resources/js/Pages/giflibrary/Library.vue
@@ -20,14 +20,16 @@ import { Head } from '@inertiajs/vue3';
         <div class="px-8">
             <div class="pt-4 grid grid-cols-1 md:grid-cols-2 gap-8">
                 <div class="pt-4 pt-4 flex justify-end">
-                    <div class="p-4 w-96 h-72 bg-white shadow-sm sm:rounded-lg text-neutral-900 flex justify-center items-center">
-                        Your GIFs!
+                    <div class="p-4 flex justify-center items-center">
+                        Your GIFs-
+                        <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="GIF 1" class="h-60 w-60 object-cover object-center lg:h-60 lg:w-60">
                     </div>
                 </div>
 
                 <div class="pt-4 pt-4 flex justify-start">
-                    <div class="p-4 w-96 h-72 bg-white shadow-sm sm:rounded-lg text-neutral-900 flex justify-center items-center">
-                        Popular GIFs!
+                    <div class="p-4 flex justify-center items-center">
+                        Popular GIFs-
+                        <img src="https://tailwindui.com/img/ecommerce-images/product-page-01-related-product-01.jpg" alt="GIF 2" class="h-60 w-60 object-cover object-center lg:h-60 lg:w-60">
                     </div>
                 </div>
             </div>
